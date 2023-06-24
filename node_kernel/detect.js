213d2b3d34378d8f7db35f70a3dcf8fda3a8de61
srliath <Santos.jonathan.jj@gmail.com>
2023-06-24 19:40:05 -0300

    modified:   ../.gitignore 	modified:   detect.js 	json_bots/ 	../server/

--- node_kernel/detect.js
+++ node_kernel/detect.js
@@ -1,53 +1,32 @@
-const { exec } = require('child_process');
-const express = require('express');
-const { MongoClient } = require('mongodb');
+const http = require('http');
+const { spawn } = require('child_process');
 
-const app = express();
-app.use(express.json());
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST') {
+    let data = '';
+    req.on('data', chunk => {
+      data += chunk;
+    });
 
-const MONGODB_URI = 'mongodb://localhost:27017'; 
-const DATABASE_NAME = 'nome_do_banco'; 
-const COLLECTION_NAME = 'nome_da_colecao'; 
+    req.on('end', () => {
+      const parsedData = JSON.parse(data);
+      const texto = parsedData.data?.texto;
+      res.end(texto || '');
+      console.log(texto);
+      // Caminho para o executável que deseja executar
+const caminhoExecutavel = 'exe/detect.exe';
 
-app.post('/', async (req, res) => {
-  const item = req.body.item;
+// Argumentos para passar ao executável
+const args = ['-n', texto];
 
-  const filePath = __dirname + '/exe/detect.exe';
-  const command = `"${filePath}" -n ${item}`;
+// Executando o executável com os argumentos usando spawn
+const processo = spawn(caminhoExecutavel, args);
 
-  exec(command, async (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Erro  ${error.message}`);
-      return res.status(500).send('Erro');
-    }
-    if (stderr) {
-      console.error(`Erro ao executar o arquivo detect.exe: ${stderr}`);
-      return res.status(500).send('Erro');
-    }
-
-    console.log(`Resultado: ${stdout}`);
-
-    const jsonData = {
-      item: item,
-      output: stdout.trim()
-    };
-
-    try {
-      const client = await MongoClient.connect(MONGODB_URI);
-      const db = client.db(DATABASE_NAME);
-      const collection = db.collection(COLLECTION_NAME);
-
-      const result = await collection.insertOne(jsonData);
-      console.log('Dados inseridos no MongoDB:', result.insertedId);
-
-      res.status(200).send('Arquivo detect.exe executado e dados inseridos com sucesso.');
-    } catch (error) {
-      console.error('Erro ao inserir os dados no MongoDB:', error);
-      res.status(500).send('Erro');
-    }
-  });
+    });
+  }
 });
 
-app.listen(3000, () => {
-  console.log('Servidor iniciado na porta 3000');
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Servidor escutando na porta ${port}`);
 });
