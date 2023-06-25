012d1582864e010d508f0e72fdbe1573074719ea
srliath <Santos.jonathan.jj@gmail.com>
2023-06-25 19:47:33 -0300

    modified:   node_kernel/detect.js 	modified:   node_modules/.package-lock.json 	modified:   package-lock.json 	modified:   package.json server/

--- node_kernel/detect.js
+++ node_kernel/detect.js
@@ -1,5 +1,35 @@
 const http = require('http');
 const { spawn } = require('child_process');
+const { MongoClient } = require('mongodb');
+const keypress = require('keypress');
+const fs = require('fs');
+
+const url = 'mongodb://fastmongoadmin:T5r4e3t5r4e3MoNg0F4@localhost:27017/';
+const dbName = 'polibot';
+const collectionName = 'user';
+//espera 0.1 segs
+function wait() {
+  return new Promise(resolve => {
+    setTimeout(resolve, 100);
+  });
+}
+
+//insere os dados no mongo
+async function insertFileContent(filePath, fileContent) {
+  const client = new MongoClient(url);
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    const result = await collection.insertOne({ content: fileContent });
+    console.log(`${result.insertedCount} documento(s) inserido(s) com sucesso`);
+  } catch (error) {
+    console.error('Ocorreu um erro:', error);
+  } finally {
+    await client.close();
+    console.log('Conexão encerrada');
+  }
+}
 
 const server = http.createServer((req, res) => {
   if (req.method === 'POST') {
@@ -13,15 +43,30 @@ const server = http.createServer((req, res) => {
       const texto = parsedData.data?.texto;
       res.end(texto || '');
       console.log(texto);
+
       // Caminho para o executável que deseja executar
-const caminhoExecutavel = 'exe/detect.exe';
+      const caminhoExecutavel = 'exe/detect.exe';
 
-// Argumentos para passar ao executável
-const args = ['-n', texto];
+      // flags para passar ao executável
+      const args = ['-n', texto];
 
-// Executando o executável com os argumentos usando spawn
-const processo = spawn(caminhoExecutavel, args);
+      // Executando o executável com os argumentos usando spawn
+      const processo = spawn(caminhoExecutavel, args);
 
+      // Aguardar o usuário pressionar a tecla "Esc"
+      keypress(process.stdin);
+      process.stdin.setRawMode(true);
+      process.stdin.resume();
+      process.stdin.on('keypress', (ch, key) => {
+        if (key && key.name === 'escape') {
+          wait();
+          const filePath = './json_bots/' + texto + '.json';
+          const fileContent = fs.readFileSync(filePath, 'utf8');
+          insertFileContent(filePath, fileContent);
+
+          process.stdin.pause();
+        }
+      });
     });
   }
 });
