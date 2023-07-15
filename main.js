46d368baf4ed0d740b832513734e3392958f4ad9
srliath <Santos.jonathan.jj@gmail.com>
2023-07-15 15:05:11 -0300

    remove typescript is not necessary, for the project is a problem

--- main.js
+++ main.js
@@ -2,24 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var electron_1 = require("electron");
 var path = require("path");
+
 function createWindow() {
-<<<<<<< HEAD
-    var win = new electron_1.BrowserWindow({
-        width: 800,
-        height: 600,
-        frame: false,
-        webPreferences: {
-            nodeIntegration: true,
-            preload: path.join(__dirname, './windows/preload.js')
-        }
-    });
-    win.loadFile('./windows/index.html');
-    // win.webContents.openDevTools();
-=======
-  const win = new BrowserWindow({
+  var win = new electron_1.BrowserWindow({
     width: 800,
     height: 600,
-    frame: false, //remove border
+    frame: false,
     webPreferences: {
       nodeIntegration: true,
       preload: path.join(__dirname, 'preload.js')
@@ -27,33 +15,20 @@ function createWindow() {
   });
 
   win.loadFile('windows/main.html');
-<<<<<<< HEAD
->>>>>>> parent of 636fe55 (Merge branch 'main' of https://github.com/SrLiath/Polibot)
-=======
->>>>>>> parent of 636fe55 (Merge branch 'main' of https://github.com/SrLiath/Polibot)
 }
-electron_1.app.whenReady().then(function () {
-    createWindow();
-    electron_1.app.on('activate', function () {
-        if (electron_1.BrowserWindow.getAllWindows().length === 0) {
-            createWindow();
-        }
-    });
-});
-electron_1.app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') {
-        electron_1.app.quit();
-    }
-});
-<<<<<<< HEAD
-=======
 
-app.on('window-all-closed', () => {
+electron_1.app.whenReady().then(function () {
+  createWindow();
+
+  electron_1.app.on('activate', function () {
+    if (electron_1.BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+electron_1.app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
-    app.quit();
+    electron_1.app.quit();
   }
 });
-<<<<<<< HEAD
->>>>>>> parent of 636fe55 (Merge branch 'main' of https://github.com/SrLiath/Polibot)
-=======
->>>>>>> parent of 636fe55 (Merge branch 'main' of https://github.com/SrLiath/Polibot)
