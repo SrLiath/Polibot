a78c1a6b376a048ce1176366c00ce0a235be932d
Renan <baratellarenan@gmail.com>
2024-06-10 18:04:16 -0300

    unificado js e adicionado requirements.txt

--- pages/js/py_api.js
+++ pages/js/py_api.js
@@ -51,6 +51,7 @@ function convertKey(key) {
     const final = hexToLetter[cleanedKey] ? "CTRL + " + hexToLetter[cleanedKey].toUpperCase() : undefined
     return final || key;
 }
+
 window.receiveData = function (data) {
     const listbots = document.getElementById('listBots')
     const atalhoNomeBot = document.getElementById('atalhoNomeBot')
@@ -98,10 +99,6 @@ window.receiveData = function (data) {
 
                 }
             });
-
-
-
-            // alert(bot['botname'] + ' deletado com sucesso')
         })
 
         buttonExcluir.appendChild(tagI)
@@ -123,7 +120,6 @@ window.receiveData = function (data) {
 
 document.getElementById('close-btn').addEventListener('click', function () {
     window.pywebview.api.close()
-
 })
 
 document.getElementById('minimize-btn').addEventListener('click', function () {
@@ -241,3 +237,68 @@ function iniciarBot() {
     window.pywebview.api.detect(headers)
     window.pywebview.api.minimize()
 }
+
+function abrirTela(tela) {
+    // Esconder todas as telas
+    let telas = document.querySelectorAll('.content div');
+    telas.forEach(element => {
+      element.classList.add('d-none');
+    });
+  
+    // Mostrar a tela selecionada
+    let telaSelecionada = document.getElementById(tela);
+    telaSelecionada.classList.remove('d-none');
+  
+    telaSelecionada.children[1].children[0].classList.remove('d-none')
+  
+    console.log(telaSelecionada)
+  
+    if(telaSelecionada.id === 'tela1'){
+      telaSelecionada.children[2].children[0].classList.remove('d-none')
+      telaSelecionada.children[2].children[0].children[0].classList.remove('d-none')
+    }
+  
+  
+    for (let i = 0; i < telaSelecionada.children[1].children[0].children.length; i++) {
+      telaSelecionada.children[1].children[0].children[i].classList.remove('d-none')
+    }
+  
+    for (let i = 0; i < telaSelecionada.children.length; i++) {
+      if (telaSelecionada.children[i].tagName == 'DIV') {
+        telaSelecionada.children[i].classList.remove('d-none')
+      }
+    }
+  }
+  
+  function openModal() { //chama no html
+    const submitBtn = document.getElementById("submitBtn");
+    const closeModalBtn = document.getElementById("closeModalBtn")
+    modal.style.display = "flex";
+  
+    closeModalBtn.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
+    submitBtn.addEventListener("click", (e) => {
+      modal.style.display = "none";
+      gravarBot()
+    })
+  
+    opcao.addEventListener('change', () => {
+      var allDivs = document.querySelectorAll('.selected');
+      allDivs.forEach(div => div.style.display = 'none');
+      document.getElementById(opcao.value).style.display = 'block';
+    });
+  
+    
+  
+  }
+  
+  //evento se clikar fora do modal o modal fecha
+  window.addEventListener('click', event => {
+    const modal = document.getElementById("modal");
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  });
+  
+  
